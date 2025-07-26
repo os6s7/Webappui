c46a74ac45ee14092fa09da4aa6de4356fe3932b
Os6s77 <125276329+os6s7@users.noreply.github.com>
2025-07-26 19:36:44 +0300

    Update vite.config.js

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -5,14 +5,14 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: process.env.PORT || 3000,
-    strictPort: true,
+    strictPort: true
   },
   preview: {
     port: process.env.PORT || 3000,
-    strictPort: true,
+    strictPort: true
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    emptyOutDir: true
   }
 });
