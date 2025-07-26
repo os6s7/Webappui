7e7e4f2e94f58c40ace83deb031f56acb503f794
Os6s77 <125276329+os6s7@users.noreply.github.com>
2025-07-26 19:49:22 +0300

    Update App.vue

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,16 +1,4 @@
-<template>
-  <h1 style="text-align: center; margin-top: 50px;">
-    🎉 Hello from Vue 3 + Vite!
-  </h1>
-</template>
+import { createApp } from 'vue';
+import App from './App.vue';
 
-<script setup>
-// nothing yet
-</script>
-
-<style>
-body {
-  font-family: sans-serif;
-  background: #f0f0f0;
-}
-</style>
+createApp(App).mount('#app');
