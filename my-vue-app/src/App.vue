97bf24ea645cb1a976b19135ecd5702d2c813fbd
Os6s77 <125276329+os6s7@users.noreply.github.com>
2025-07-26 19:38:38 +0300

    Update App.vue

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    <h1>مرحبا من Vue 3!</h1>
-  </div>
+  <h1 style="text-align: center; margin-top: 50px;">
+    🎉 Hello from Vue 3 + Vite!
+  </h1>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
+// nothing yet
 </script>
+
+<style>
+body {
+  font-family: sans-serif;
+  background: #f0f0f0;
+}
+</style>
