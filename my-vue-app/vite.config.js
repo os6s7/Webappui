539671e7c57174508c7bd7f6b032ed300221f84f
Os6s77 <125276329+os6s7@users.noreply.github.com>
2025-07-26 19:51:38 +0300

    Update vite.config.js

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -3,16 +3,17 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  root: './src',           // خلي المجلد الأساسي src
+  build: {
+    outDir: '../dist',     // خلي مجلد الخرج برا src حتى يظل بالمستوى الأعلى
+    emptyOutDir: true,
+  },
   server: {
     port: process.env.PORT || 3000,
-    strictPort: true
+    strictPort: true,
   },
   preview: {
     port: process.env.PORT || 3000,
-    strictPort: true
+    strictPort: true,
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
 });
