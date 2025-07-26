c90f264b7374713067bb6d8e2f042ba6765594f8
Os6s77 <125276329+os6s7@users.noreply.github.com>
2025-07-26 14:41:30 +0000

    init: upload Vue project to GitHub

---
+++ main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+
+createApp({
+  data() {
+    return {
+      message: 'هلا والله من Vue على Replit!'
+    }
+  }
+}).mount('#app');
---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="text-align: center; margin-top: 50px;">
+    <h1>مرحباً بك في متجر التيليجرام</h1>
+    <p>هذا هو ميني أب الخاص بك</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
